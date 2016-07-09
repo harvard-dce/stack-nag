b89a7b638b66186ffa2a41718435dbface1c8890
Jay Luker <jay_luker@harvard.edu>
2016-07-09 05:34:34 -0400

    publish cloudwatch metrics.
    includes:
    * check event for "action" type
    * publish number of running stacks metric if action type is "metrics"

--- function.py
+++ function.py
@@ -11,7 +11,13 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
 
+    logger.info("Event received: %s", str(event))
+
+    if 'action' not in event:
+        raise RuntimeError("Recieved invalid event")
+
     opsworks = boto3.client('opsworks')
+    cw = boto3.client('cloudwatch')
 
     config = ConfigParser()
     config.read('config.cfg')
@@ -36,12 +42,25 @@ def lambda_handler(event, context):
         if running_instances:
             running_stacks.append((stack['Name'], len(running_instances)))
 
-    if running_stacks:
-        post_message("%d currently running dev clusters:" % len(running_stacks))
-        for s in running_stacks:
-            post_message("%s: %d instance%s" % (s[0], s[1], s[1] > 1 and "s" or ""))
-    else:
-        post_message("No running dev clusters")
+    if event['action'] == 'post':
+        if running_stacks:
+            post_message("%d currently running dev clusters:" % len(running_stacks))
+            for s in running_stacks:
+                post_message("%s: %d instance%s" % (s[0], s[1], s[1] > 1 and "s" or ""))
+        else:
+            post_message("No running dev clusters")
+    elif event['action'] == 'metrics':
+        # publish metrics
+        cw.put_metric_data(
+            Namespace='StackNag',
+            MetricData=[
+                {
+                    'MetricName': 'running_clusters',
+                    'Value': len(running_stacks),
+                    'Unit': 'Count'
+                }
+            ]
+        )
 
 
 
