dcb3b4ce14079718fe56af529b8a4b525a6b843a
Jay Luker <jay_luker@harvard.edu>
2022-04-26 14:22:12 -0400

    adding project tools: black, flake8 and pre-commit hook

--- stack-nag.py
+++ stack-nag.py
@@ -10,32 +10,33 @@ from os import getenv as env
 import time
 
 import logging
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 AWS_REGION = os.environ.get("AWS_DEFAULT_REGION", "us-east-1")
 
-if 'AWS_DEFAULT_PROFILE' in os.environ:
+if "AWS_DEFAULT_PROFILE" in os.environ:
     boto3.setup_default_session(
-        profile_name=os.environ['AWS_DEFAULT_PROFILE'],
+        profile_name=os.environ["AWS_DEFAULT_PROFILE"],
         region_name=AWS_REGION,
     )
 
-opsworks = boto3.client('opsworks')
-cw = boto3.client('cloudwatch')
-ec2 = boto3.client('ec2')
-rds = boto3.client('rds')
-s3 = boto3.resource('s3')
+opsworks = boto3.client("opsworks")
+cw = boto3.client("cloudwatch")
+ec2 = boto3.client("ec2")
+rds = boto3.client("rds")
+s3 = boto3.resource("s3")
 PRICE_NOTIFY_URL = env("PRICE_NOTIFY_URL")
 CODEBUILD_NOTIFY_URL = env("CODEBUILD_NOTIFY_URL")
-NAMESPACE = env('NAMESPACE')
+NAMESPACE = env("NAMESPACE")
 
 YELLOW = "#EBB424"
 GREEN = "#49C39E"
 RED = "#e62727"
 
 try:
-    with open('price_index.json', 'r') as f:
+    with open("price_index.json", "r") as f:
         price_index = json.load(f)
 except IOError:
     raise RuntimeError("Price index is missing. Did you run `fab generate_index`?")
@@ -45,14 +46,15 @@ def instance_price(service, instance_type):
     logger.debug(price_index)
     return price_index[service][instance_type]
 
+
 # get all this up-front as it's rather expensive
 BUCKET_TAG_INDEX = {}
 for bucket in s3.buckets.all():
     logger.debug("Generating bucket tag index")
     try:
         for tag in bucket.Tagging().tag_set:
-            if tag['Key'] == 'opsworks:stack':
-                stack_name = tag['Value']
+            if tag["Key"] == "opsworks:stack":
+                stack_name = tag["Value"]
                 BUCKET_TAG_INDEX.setdefault(stack_name, [])
                 BUCKET_TAG_INDEX[stack_name].append(bucket)
     except ClientError:
@@ -65,86 +67,60 @@ def handler(event, context):
 
     stacks = []
 
-    for s in opsworks.describe_stacks()['Stacks']:
+    for s in opsworks.describe_stacks()["Stacks"]:
         stack = Stack(s)
         stacks.append(stack)
 
     stack_names = [stack.Name for stack in stacks]
-    logger.info("Found stacks: {}".format(', '.join(stack_names)))
+    logger.info("Found stacks: {}".format(", ".join(stack_names)))
 
     running_stacks = [x for x in stacks if x.online_instances]
 
-    if 'action' in event and event['action'] == 'post stack status':
+    if "action" in event and event["action"] == "post stack status":
         if running_stacks:
             msg = "{} currently running stacks:\n".format(len(running_stacks))
             for stack in running_stacks:
-                msg += "{}: {} instance{}, ${:.2f}/hr\n"\
-                       .format(stack.Name,
-                               len(stack.online_instances),
-                               len(stack.online_instances) > 1 and "s" or "",
-                               stack.hourly_cost())
+                msg += "{}: {} instance{}, ${:.2f}/hr\n".format(
+                    stack.Name,
+                    len(stack.online_instances),
+                    len(stack.online_instances) > 1 and "s" or "",
+                    stack.hourly_cost(),
+                )
         else:
             msg = "No running stacks\n"
 
         total_hourly_cost = sum(s.hourly_cost() for s in stacks)
-        msg += "Total usage cost (including non-running stacks): ${:.2f}/hr"\
-               .format(total_hourly_cost)
+        msg += "Total usage cost (including non-running stacks): ${:.2f}/hr".format(total_hourly_cost)
 
         post_message(msg, PRICE_NOTIFY_URL, color=YELLOW)
 
-    elif 'action' in event and event['action'] == 'metrics':
+    elif "action" in event and event["action"] == "metrics":
 
         logger.info("Logging metrics to namespace: {}".format(NAMESPACE))
 
         # publish general metrics
         metric_data = [
-                {
-                    'MetricName': 'running_clusters',
-                    'Value': len(running_stacks),
-                    'Unit': 'Count'
-                },
-                {
-                    'MetricName': 'total_clusters',
-                    'Value': len(stacks),
-                    'Unit': 'Count'
-                },
-                {
-                    'MetricName': 'ec2_hourly_costs',
-                    'Value': sum(s.ec2_hourly_cost() for s in stacks)
-                },
-                {
-                    'MetricName': 'rds_hourly_costs',
-                    'Value': sum(s.rds_hourly_cost() for s in stacks)
-                },
-                {
-                    'MetricName': 'ebs_hourly_costs',
-                    'Value': sum(s.ebs_hourly_cost() for s in stacks)
-                },
-                {
-                    'MetricName': 's3_hourly_costs',
-                    'Value': sum(s.s3_hourly_cost() for s in stacks)
-                },
-                {
-                    'MetricName': 'total_hourly_costs',
-                    'Value': sum(s.hourly_cost() for s in stacks)
-                }
-            ]
+            {"MetricName": "running_clusters", "Value": len(running_stacks), "Unit": "Count"},
+            {"MetricName": "total_clusters", "Value": len(stacks), "Unit": "Count"},
+            {"MetricName": "ec2_hourly_costs", "Value": sum(s.ec2_hourly_cost() for s in stacks)},
+            {"MetricName": "rds_hourly_costs", "Value": sum(s.rds_hourly_cost() for s in stacks)},
+            {"MetricName": "ebs_hourly_costs", "Value": sum(s.ebs_hourly_cost() for s in stacks)},
+            {"MetricName": "s3_hourly_costs", "Value": sum(s.s3_hourly_cost() for s in stacks)},
+            {"MetricName": "total_hourly_costs", "Value": sum(s.hourly_cost() for s in stacks)},
+        ]
 
         for metric in metric_data:
             metric["Timestamp"] = str(time.time())
 
-        cw.put_metric_data(
-            Namespace=NAMESPACE,
-            MetricData=metric_data
-        )
+        cw.put_metric_data(Namespace=NAMESPACE, MetricData=metric_data)
 
         # publish individual metrics
         for s in stacks:
             publish_metrics(s)
 
-    elif 'source' in event and event['source'] == 'aws.codebuild':
-        project_name = event['detail']['project-name']
-        revision = event['detail']['additional-information']['source-version']
+    elif "source" in event and event["source"] == "aws.codebuild":
+        project_name = event["detail"]["project-name"]
+        revision = event["detail"]["additional-information"]["source-version"]
 
         build_id = event["detail"]["build-id"]
         build_id_parts = build_id.split(":", 5)
@@ -153,21 +129,21 @@ def handler(event, context):
         build_path = build_id_parts[5]
         build_link = f"https://{build_region}.console.aws.amazon.com/codesuite/codebuild/{build_account}/projects/{project_name}/{build_path}"
 
-        if event['detail']['current-phase'] == "SUBMITTED":
+        if event["detail"]["current-phase"] == "SUBMITTED":
             msg = f"CodeBuild submitted for <{build_link}|{project_name}@{revision}>"
             status = None
-        elif event['detail']['current-phase'] == "COMPLETED":
-            status = event['detail']['build-status']
+        elif event["detail"]["current-phase"] == "COMPLETED":
+            status = event["detail"]["build-status"]
             msg = f"CodeBuild complete for <{build_link}|{project_name}@{revision}>, status: {status}"
         else:
             raise RuntimeError("Received invalid event: {}".format(event))
 
         if status == "FAILED":
-          status_color = RED
+            status_color = RED
         elif status == "SUCCEEDED":
-          status_color = GREEN
+            status_color = GREEN
         else:
-          status_color = YELLOW
+            status_color = YELLOW
 
         post_message(msg, CODEBUILD_NOTIFY_URL, color=status_color)
 
@@ -176,7 +152,6 @@ def handler(event, context):
 
 
 class Stack(object):
-
     def __init__(self, data):
         self.data = data
 
@@ -188,9 +163,9 @@ class Stack(object):
 
     @property
     def instances(self):
-        if not hasattr(self, '_instances'):
+        if not hasattr(self, "_instances"):
             try:
-                self._instances = opsworks.describe_instances(StackId=self.StackId)['Instances']
+                self._instances = opsworks.describe_instances(StackId=self.StackId)["Instances"]
             except KeyError:
                 logger.warning("No instances found for stack {}".format(self.Name))
                 self._instances = []
@@ -200,16 +175,16 @@ class Stack(object):
     def online_instances(self):
         for x in self.instances:
             logger.debug("Instance detail: {}".format(x))
-        return [x for x in self.instances if x['Status'] == 'online']
+        return [x for x in self.instances if x["Status"] == "online"]
 
     @property
     def rds_instance(self):
-        if not hasattr(self, '_rds'):
+        if not hasattr(self, "_rds"):
             try:
-                opsworks_db = opsworks.describe_rds_db_instances(StackId=self.StackId)['RdsDbInstances'][0]
-                self._rds = rds.describe_db_instances(
-                    DBInstanceIdentifier=opsworks_db['DbInstanceIdentifier']
-                )['DBInstances'][0]
+                opsworks_db = opsworks.describe_rds_db_instances(StackId=self.StackId)["RdsDbInstances"][0]
+                self._rds = rds.describe_db_instances(DBInstanceIdentifier=opsworks_db["DbInstanceIdentifier"])[
+                    "DBInstances"
+                ][0]
             except (ClientError, IndexError):
                 logger.debug("No rds instance found for stack {}".format(self.Name))
                 self._rds = None
@@ -217,16 +192,16 @@ class Stack(object):
 
     @property
     def volumes(self):
-        if not hasattr(self, '_volumes'):
+        if not hasattr(self, "_volumes"):
             try:
-                self._volumes = opsworks.describe_volumes(StackId=self.StackId)['Volumes']
+                self._volumes = opsworks.describe_volumes(StackId=self.StackId)["Volumes"]
             except KeyError:
                 self._volumes = []
         return self._volumes
 
     @property
     def buckets(self):
-        if not hasattr(self, '_buckets'):
+        if not hasattr(self, "_buckets"):
             try:
                 self._buckets = BUCKET_TAG_INDEX[self.Name]
             except KeyError:
@@ -235,40 +210,40 @@ class Stack(object):
 
     def get_bucket_size(self, bucket_name):
         resp = cw.get_metric_statistics(
-            Namespace='AWS/S3',
-            MetricName='BucketSizeBytes',
+            Namespace="AWS/S3",
+            MetricName="BucketSizeBytes",
             Dimensions=[
-                {'Name': 'BucketName', 'Value': bucket_name},
-                {'Name': 'StorageType', 'Value': 'StandardStorage'}
+                {"Name": "BucketName", "Value": bucket_name},
+                {"Name": "StorageType", "Value": "StandardStorage"},
             ],
             StartTime=datetime.now() - timedelta(days=1),
             EndTime=datetime.now(),
             Period=86400,
-            Statistics=['Average']
+            Statistics=["Average"],
         )
         try:
-            if len(resp['Datapoints']) == 0:
+            if len(resp["Datapoints"]) == 0:
                 # no items in bucket
                 return 0
             else:
-                return resp['Datapoints'][-1]['Average']
+                return resp["Datapoints"][-1]["Average"]
         except IndexError:
             logger.warning("Failed to get size for bucket '{}'".format(bucket_name))
             return 0
 
     def shortname(self):
-        return re.sub('[^a-z\d\-]', '-', self.Name)
+        return re.sub(r"[^a-z\d\-]", "-", self.Name)
 
     def ec2_hourly_cost(self):
-        return sum(instance_price('ec2', x['InstanceType']) for x in self.online_instances)
+        return sum(instance_price("ec2", x["InstanceType"]) for x in self.online_instances)
 
     def rds_hourly_cost(self):
         if self.rds_instance is not None:
-            return instance_price('rds', self.rds_instance['DBInstanceClass'])
+            return instance_price("rds", self.rds_instance["DBInstanceClass"])
         return 0
 
     def ebs_hourly_cost(self):
-        total_size = sum(x['Size'] for x in self.volumes)
+        total_size = sum(x["Size"] for x in self.volumes)
         return (total_size * 0.10) / (30 * 24)
 
     def s3_hourly_cost(self):
@@ -277,21 +252,14 @@ class Stack(object):
         return (total_gb * 0.03) / (30 * 24)
 
     def hourly_cost(self):
-        return sum([
-            self.ec2_hourly_cost(),
-            self.rds_hourly_cost(),
-            self.ebs_hourly_cost(),
-            self.s3_hourly_cost()
-        ])
+        return sum([self.ec2_hourly_cost(), self.rds_hourly_cost(), self.ebs_hourly_cost(), self.s3_hourly_cost()])
 
 
 def post_message(msg, notify_url, color):
-    req_body = {'attachments': [{'color': color, 'text': msg}]}
+    req_body = {"attachments": [{"color": color, "text": msg}]}
     logger.info("using notify_url: {}".format(notify_url))
     logger.info("posting message: {}".format(msg))
-    r = requests.post(notify_url,
-                      headers={'Content-Type': 'application/json'},
-                      json=req_body)
+    r = requests.post(notify_url, headers={"Content-Type": "application/json"}, json=req_body)
     logger.info("Notify url status code: {}".format(r.status_code))
 
 
@@ -300,44 +268,26 @@ def publish_metrics(stack):
     logger.debug("publish metrics for {}".format(stack.Name))
 
     metric_data = [
-            {
-                'MetricName': 'ec2_hourly_costs'.format(stack.Name),
-                'Value': stack.ec2_hourly_cost()
-            },
-            {
-                'MetricName': 'rds_hourly_costs'.format(stack.Name),
-                'Value': stack.rds_hourly_cost()
-            },
-            {
-                'MetricName': 'ebs_hourly_costs'.format(stack.Name),
-                'Value': stack.ebs_hourly_cost()
-            },
-            {
-                'MetricName': 's3_hourly_costs'.format(stack.Name),
-                'Value': stack.s3_hourly_cost()
-            },
-            {
-                'MetricName': 'total_hourly_costs'.format(stack.Name),
-                'Value': stack.hourly_cost()
-            }
-        ]
+        {"MetricName": "ec2_hourly_costs", "Value": stack.ec2_hourly_cost()},
+        {"MetricName": "rds_hourly_costs", "Value": stack.rds_hourly_cost()},
+        {"MetricName": "ebs_hourly_costs", "Value": stack.ebs_hourly_cost()},
+        {"MetricName": "s3_hourly_costs", "Value": stack.s3_hourly_cost()},
+        {"MetricName": "total_hourly_costs", "Value": stack.hourly_cost()},
+    ]
 
     for metric in metric_data:
         metric["Dimensions"] = [{"Name": stack.Name, "Value": stack.Name}]
         metric["Timestamp"] = str(time.time())
 
-    cw.put_metric_data(
-        Namespace=NAMESPACE,
-        MetricData=metric_data
-    )
+    cw.put_metric_data(Namespace=NAMESPACE, MetricData=metric_data)
 
 
 # for local testing
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--action', type=str)
+    parser.add_argument("--action", type=str)
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
-    handler({'action': args.action}, None)
+    handler({"action": args.action}, None)
--- tasks.py
+++ tasks.py
@@ -6,12 +6,11 @@ from os.path import join, dirname, exists
 from dotenv import load_dotenv
 import json
 import time
-import sys
 
-load_dotenv(join(dirname(__file__), '.env'))
+load_dotenv(join(dirname(__file__), ".env"))
 
-STACK_NAME = env('STACK_NAME')
-AWS_PROFILE = env('AWS_PROFILE')
+STACK_NAME = env("STACK_NAME")
+AWS_PROFILE = env("AWS_PROFILE")
 
 
 @task
@@ -19,12 +18,11 @@ def create(ctx):
     """
     Generate price index and create CloudFormation stack
     """
-    code_bucket = getenv('LAMBDA_CODE_BUCKET')
+    code_bucket = getenv("LAMBDA_CODE_BUCKET")
     cmd = "aws {} s3 ls {}".format(profile_arg(), code_bucket)
     exists = ctx.run(cmd, hide=True, warn=True)
     if not exists.ok:
-        print("Lambda code bucket does not exist. "
-              "Specify an existing S3 bucket as the \"LAMBDA_CODE_BUCKET.\"")
+        print("Lambda code bucket does not exist. " 'Specify an existing S3 bucket as the "LAMBDA_CODE_BUCKET."')
         return
 
     __generate_index(ctx)
@@ -39,26 +37,23 @@ def update(ctx):
 @task
 def update_lambda(ctx):
     __package(ctx)
-    ctx.run("aws {} lambda update-function-code "
-            "--function-name {}-function --s3-bucket {} --s3-key {}/stack-nag.zip"
-            .format(profile_arg(),
-                    STACK_NAME,
-                    getenv('LAMBDA_CODE_BUCKET'),
-                    STACK_NAME)
-            )
+    ctx.run(
+        "aws {} lambda update-function-code "
+        "--function-name {}-function --s3-bucket {} --s3-key {}/stack-nag.zip".format(
+            profile_arg(), STACK_NAME, getenv("LAMBDA_CODE_BUCKET"), STACK_NAME
+        )
+    )
 
 
 @task
 def delete(ctx):
-    cmd = "aws {} cloudformation delete-stack --stack-name {}"\
-        .format(profile_arg(), STACK_NAME)
+    cmd = "aws {} cloudformation delete-stack --stack-name {}".format(profile_arg(), STACK_NAME)
     res = ctx.run(cmd)
 
     if res.exited == 0:
         __wait_for(ctx, "stack-delete-complete")
 
-    cmd = "aws {} s3 rm s3://{}/{}/stack-nag.zip"\
-        .format(profile_arg(), getenv("LAMBDA_CODE_BUCKET"), STACK_NAME)
+    cmd = "aws {} s3 rm s3://{}/{}/stack-nag.zip".format(profile_arg(), getenv("LAMBDA_CODE_BUCKET"), STACK_NAME)
     ctx.run(cmd)
 
 
@@ -74,7 +69,7 @@ def refresh_index(ctx):
 ns = Collection()
 ns.add_task(refresh_index)
 
-stack_ns = Collection('stack')
+stack_ns = Collection("stack")
 stack_ns.add_task(create)
 stack_ns.add_task(update)
 stack_ns.add_task(delete)
@@ -91,8 +86,7 @@ def getenv(var, required=True):
 
 
 def stack_exists(ctx):
-    cmd = "aws {} cloudformation describe-stacks --stack-name {}"\
-          .format(profile_arg(), STACK_NAME)
+    cmd = "aws {} cloudformation describe-stacks --stack-name {}".format(profile_arg(), STACK_NAME)
     res = ctx.run(cmd, hide=True, warn=True, echo=False)
     return res.exited == 0
 
@@ -110,33 +104,35 @@ def __create_or_update(ctx, op):
         __package(ctx)
 
         if op == "create-change-set":
-          change_set_name_arg = f"--change-set-name stack-nag-{str(int(time.time()))} "
-          wait_for_op = "change-set-create-complete"
+            change_set_name_arg = f"--change-set-name stack-nag-{str(int(time.time()))} "
+            wait_for_op = "change-set-create-complete"
         else:
-          change_set_name_arg = ""
-          wait_for_op = "stack-create-complete"
+            change_set_name_arg = ""
+            wait_for_op = "stack-create-complete"
 
-        cmd = ("aws {} cloudformation {} "
-               "--capabilities CAPABILITY_NAMED_IAM "
-               "--stack-name {} "
-               "{}"
-               "--template-body file://template.yml "
-               "--parameters "
-               "ParameterKey=LambdaCodeBucket,ParameterValue={} "
-               "ParameterKey=PriceNotifyUrl,ParameterValue={} "
-               "ParameterKey=CodeBuildNotifyUrl,ParameterValue={} "
-               "ParameterKey=CWNamespace,ParameterValue={} "
-               "ParameterKey=NotifyScheduleExpression,ParameterValue=\"{}\""
-               .format(profile_arg(),
-                       op,
-                       STACK_NAME,
-                       change_set_name_arg,
-                       getenv('LAMBDA_CODE_BUCKET'),
-                       getenv('PRICE_NOTIFY_URL'),
-                       getenv('CODEBUILD_NOTIFY_URL'),
-                       getenv('CLOUDWATCH_NAMESPACE'),
-                       getenv('NOTIFY_SCHEDULE_EXPRESSION').replace(',', 'x'))
-               )
+        cmd = (
+            "aws {} cloudformation {} "
+            "--capabilities CAPABILITY_NAMED_IAM "
+            "--stack-name {} "
+            "{}"
+            "--template-body file://template.yml "
+            "--parameters "
+            "ParameterKey=LambdaCodeBucket,ParameterValue={} "
+            "ParameterKey=PriceNotifyUrl,ParameterValue={} "
+            "ParameterKey=CodeBuildNotifyUrl,ParameterValue={} "
+            "ParameterKey=CWNamespace,ParameterValue={} "
+            'ParameterKey=NotifyScheduleExpression,ParameterValue="{}"'.format(
+                profile_arg(),
+                op,
+                STACK_NAME,
+                change_set_name_arg,
+                getenv("LAMBDA_CODE_BUCKET"),
+                getenv("PRICE_NOTIFY_URL"),
+                getenv("CODEBUILD_NOTIFY_URL"),
+                getenv("CLOUDWATCH_NAMESPACE"),
+                getenv("NOTIFY_SCHEDULE_EXPRESSION").replace(",", "x"),
+            )
+        )
 
         res = ctx.run(cmd)
 
@@ -148,11 +144,11 @@ def __package(ctx):
 
     func = "stack-nag"
 
-    req_file = join(dirname(__file__), 'function-requirements.txt')
+    req_file = join(dirname(__file__), "function-requirements.txt")
 
-    zip_path = join(dirname(__file__), 'dist/{}.zip'.format(func))
+    zip_path = join(dirname(__file__), "dist/{}.zip".format(func))
 
-    build_path = join(dirname(__file__), 'dist')
+    build_path = join(dirname(__file__), "dist")
 
     if exists(build_path):
         shutil.rmtree(build_path)
@@ -162,8 +158,8 @@ def __package(ctx):
     else:
         ctx.run("mkdir {}".format(build_path))
 
-    module_path = join(dirname(__file__), '{}.py'.format(func))
-    module_dist_path = join(build_path, '{}.py'.format(func))
+    module_path = join(dirname(__file__), "{}.py".format(func))
+    module_dist_path = join(build_path, "{}.py".format(func))
     try:
         print("symlinking {} to {}".format(module_path, module_dist_path))
         symlink(module_path, module_dist_path)
@@ -171,20 +167,20 @@ def __package(ctx):
         pass
 
     with ctx.cd(build_path):
-        ctx.run("zip -r {} . {}".format(zip_path, '../price_index.json'))
+        ctx.run("zip -r {} . {}".format(zip_path, "../price_index.json"))
 
-    ctx.run("aws {} s3 cp {} s3://{}/{}/stack-nag.zip".format(
-        profile_arg(),
-        zip_path,
-        getenv('LAMBDA_CODE_BUCKET'),
-        STACK_NAME,
+    ctx.run(
+        "aws {} s3 cp {} s3://{}/{}/stack-nag.zip".format(
+            profile_arg(),
+            zip_path,
+            getenv("LAMBDA_CODE_BUCKET"),
+            STACK_NAME,
         )
     )
 
 
 def __wait_for(ctx, op):
-    wait_cmd = ("aws {} cloudformation wait {} "
-                "--stack-name {}").format(profile_arg(), op, STACK_NAME)
+    wait_cmd = ("aws {} cloudformation wait {} " "--stack-name {}").format(profile_arg(), op, STACK_NAME)
     print("Waiting for stack {} to complete...".format(op))
     ctx.run(wait_cmd)
     print("Done")
@@ -193,44 +189,44 @@ def __wait_for(ctx, op):
 def __generate_index(ctx):
 
     price_index = {}
-    for service, service_code in [('ec2', 'AmazonEC2'), ('rds', 'AmazonRDS')]:
+    for service, service_code in [("ec2", "AmazonEC2"), ("rds", "AmazonRDS")]:
 
-        print('Getting prices for %s...' % service)
+        print("Getting prices for %s..." % service)
 
-        cmd = ("aws {} pricing get-products "
-               "--service-code {} --filters file://filters_{}.json"
-               .format(profile_arg(), service_code, service))
+        cmd = "aws {} pricing get-products " "--service-code {} --filters file://filters_{}.json".format(
+            profile_arg(), service_code, service
+        )
 
         res = ctx.run(cmd, hide=True).stdout
-        price_list = json.loads(res)['PriceList']
+        price_list = json.loads(res)["PriceList"]
 
         price_index.setdefault(service, {})
         for price_data in price_list:
             price_data = json.loads(price_data)
 
-            product = price_data['product']
-            if 'instanceType' not in product['attributes']:
+            product = price_data["product"]
+            if "instanceType" not in product["attributes"]:
                 print("\nProduct missing instanceType: {}".format(product))
                 continue
-            if service == 'rds' and product['attributes']['engineCode'] == '210':
+            if service == "rds" and product["attributes"]["engineCode"] == "210":
                 continue
 
-            instance_type = product['attributes']['instanceType']
-            sku = product['sku']
+            instance_type = product["attributes"]["instanceType"]
+            sku = product["sku"]
 
-            if service == 'rds':
-                usage_type = product['attributes']['usagetype']
+            if service == "rds":
+                usage_type = product["attributes"]["usagetype"]
                 if not usage_type.startswith("InstanceUsage"):
                     continue
 
-            for term in price_data['terms']['OnDemand'].values():
-                if term['sku'] == sku:
-                    for price_dimension in term['priceDimensions'].values():
-                        price = float(price_dimension['pricePerUnit']['USD'])
+            for term in price_data["terms"]["OnDemand"].values():
+                if term["sku"] == sku:
+                    for price_dimension in term["priceDimensions"].values():
+                        price = float(price_dimension["pricePerUnit"]["USD"])
                         break
                 break
 
             price_index[service][instance_type] = price
 
-        with open('price_index.json', 'w') as f:
+        with open("price_index.json", "w") as f:
             json.dump(price_index, f, indent=True)
